ff1203aa99a9420a1188b1dfff917384398d4e66
Bene <br@garmisch.net>
2019-09-25 17:49:28 +0200

    functions

---
+++ functions/functions.js
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @param variable
+ * @returns {boolean}
+ *
+ * @todo ist noch ungetestet
+ */
+function isset (variable) {
+	if(typeof(variable) == "undefined" || variable == null)
+		return false;
+	else
+		return !(typeof (variable) == "object" && !variable.length);
+}
