56017f75237353e9192d8b0c837a67a0056da891
Bene <br@garmisch.net>
2019-11-22 09:39:23 +0100

    cookiezeug

--- functions/functions.js
+++ functions/functions.js
@@ -28,9 +28,10 @@ function isset(variable)
  *
  * Der gesetzte Cookie heißt cookieconsent_dismissed. Ist dessen Wert "yes" hat der User allen Cookies zugestimmt, bei "no" wurden nur essenziellen Cookies zugestimmt.
  */
-
-function cookiePopup()
+function cookiePopup(options)
 {
+	window.cookieconsent_options(options);
+
 	if (!window.hasCookieConsent)
 	{
 		window.hasCookieConsent = !0;
@@ -268,20 +269,4 @@ function cookiePopup()
 			})(), s.addEventListener(document, "readystatechange", o)
 		}
 	}
-};
-/*
-(function (w, d, s, l, i)
-{
-	w[l] = w[l] || [];
-	w[l].push({
-		'gtm.start': new Date().getTime(),
-		event: 'gtm.js'
-	});
-	var f = d.getElementsByTagName(s)[0],
-		j = d.createElement(s),
-		dl = l != 'dataLayer' ? '&l=' + l : '';
-	j.async = true;
-	j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-	f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-TVL985');
- */
+}
