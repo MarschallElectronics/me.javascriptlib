2fd664278fb9026e0599ee718c1d2d7f19b254c4
Bene <bene@zipzop.de>
2020-07-15 15:26:03 +0200

    escape

--- functions/functions.js
+++ functions/functions.js
@@ -10,3 +10,13 @@ function isset(variable)
 	else
 		return !(typeof (variable) == "object" && !variable.length);
 }
+
+/**
+ * gibt den Selector escaped für Jquery zurück
+ * @param selector
+ * @returns {*}
+ */
+function get_escaped_selector_name(selector)
+{
+	return selector.replace(/(:|\.|\[|\])/g,'\\$1');
+}
